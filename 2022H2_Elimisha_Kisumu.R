d703ab67eeac4bdfe01f7aea4579b79aaa3ba832
David Fong <vkelim@bigpond.com>
2024-02-01 17:43:08 +1100

    Elimisha Kisumu report R scripts to combine cover/body of Elimisha reports, and renumber combined file changes to allow rendering to HTML

---
+++ 2022H2_Elimisha_Kisumu.R
@@ -0,0 +1,29 @@
+# render two PDFs and combine into one PDF
+library(quarto)
+library(pdftools)
+library(fs)
+
+# render
+quarto_render( "2022H2_Elimisha_Kisumu_cover.qmd", output_format = "titlepage-pdf")
+quarto_render( "2022H2_Elimisha_Kisumu_body.qmd", output_format = "PrettyPDF-pdf")
+
+tmp_pdf_filename <- file_temp(ext = "pdf")
+
+# combine
+pdf_combine(
+  c("2022H2_Elimisha_Kisumu_cover.pdf", "2022H2_Elimisha_Kisumu_body.pdf"),
+  output = tmp_pdf_filename
+  )
+
+# renumber pages
+# first page (the cover page) is 'i', and second page is '1'
+# note that page numbers are shown correctly in evince and WPS,
+# but not programs like okular
+system(
+  paste(
+    "pdftk",
+    tmp_pdf_filename,
+    "update_info pagerenumbering_metadata.txt",
+    "output 2022H2_Elimisha_Kisumu.pdf"
+  )
+)
